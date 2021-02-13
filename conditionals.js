a75f99bf399071415a841a66efa4304c91b88416
AlePortugal <aleportugalsilva@gmail.com>
2021-02-13 20:34:56 +0000

    Use conditionals to tell people about the weather

---
+++ conditionals.js
@@ -0,0 +1,24 @@
+/*Conditional Execution    
+// to RUN type node conditionals.js 
+     
+const raining = true;
+const cold = true;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+*/
+// If..Else
+const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
